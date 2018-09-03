29c2283e0566e896e65821c4a2515192d73d11d3
Milan Radanovic <radanovic.m@hotmail.com>
2018-09-03 12:51:55 +0200

    dispecer kontroler

--- WEB1Projekat/TaxiService/Controllers/HomeController.cs
+++ WEB1Projekat/TaxiService/Controllers/HomeController.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using TaxiService.Models;
 
 namespace TaxiService.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ApiController
     {
-        public ActionResult Index()
+        public Korisnik Post([FromBody]Korisnik korisnik)
         {
-            ViewBag.Title = "Home Page";
+            Dispeceri dispeceri = (Dispeceri)HttpContext.Current.Application["dispeceri"];
+            Korisnici users = (Korisnici)HttpContext.Current.Application["korisnici"];
+            Vozaci vozaci = (Vozaci)HttpContext.Current.Application["vozaci"];
 
-            return View();
+            foreach (var item in users.korisnici)
+            {
+                if (korisnik.KorisnickoIme == item.KorisnickoIme)
+                {
+                    return item;
+                }
+            }
+
+            foreach (var item in dispeceri.dispecers)
+            {
+                if (korisnik.KorisnickoIme == item.KorisnickoIme)
+                {
+                    return item;
+                }
+            }
+
+            foreach (var item in vozaci.vozaci)
+            {
+                if (korisnik.KorisnickoIme == item.KorisnickoIme)
+                    return item;
+            }
+
+            return null;
         }
     }
 }
